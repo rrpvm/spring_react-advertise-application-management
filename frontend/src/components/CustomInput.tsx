1139986647d17a129d3d3ada4cb12c8c8ca4d83b
rrpvm <funikovsasa@gmail.com>
2022-04-29 11:00:19 +0700

    front update

---
+++ frontend/src/components/CustomInput.tsx
@@ -0,0 +1,9 @@
+export const CustomInput: React.FC = () => {
+    return (
+        <>
+            <div>
+                <label></label><input className="customInput"></input>
+            </div>
+        </>
+    );
+}
--- frontend/src/layouts/Categories.tsx
+++ frontend/src/layouts/Categories.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { CustomInput } from "../components/CustomInput";
 
-export const Banners: React.FC = () => {
+export const Categories: React.FC = () => {
     return (
         <div className="row">
             <div className="col s2 side-left-nav z-depth-1">
-                <h3 className="center-align">Banners</h3>
+                <h3 className="center-align">Categories</h3>
                 <div className="search-wrapper">
                     <label htmlFor="search"><i className="material-icons">search</i>    </label>
                     <input id="search" className="input-none-border" />
                 </div>
-                <a className="waves-effect pink lighten-2 btn-large floating-button" href="/">Create new Banner</a>
+                <a className="waves-effect pink lighten-2 btn-large floating-button" href="/">Create new category</a>
             </div>
             <div className="col s9 edit-component z-depth-2" >
                 <h3>Create new banner</h3>
