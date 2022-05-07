6f8e9e33f929101caecde4e92f713af24fac2d71
rrpvm <funikovsasa@gmail.com>
2022-05-07 14:34:51 +0700

    nothing

--- backend/src/main/java/com/rrpvm/backend/services/JdbcUserDetailsService.java
+++ backend/src/main/java/com/rrpvm/backend/services/JdbcUserDetailsService.java
@@ -2,13 +2,17 @@ package com.rrpvm.backend.services;
 
 import com.rrpvm.backend.daos.UserRepository;
 import com.rrpvm.backend.entities.User;
-import com.rrpvm.backend.exceptions.UserAlreadyExistException;
 import org.apache.catalina.core.ApplicationContext;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,18 +22,16 @@ public class JdbcUserDetailsService implements UserDetailsService, InitializingB
     private static final String ADMIN_LOGIN = "rrpvm";
     private static final String ADMIN_PASSWORD = "12345";
     @Autowired
-    private UserRepository userRepository;
+    private  UserRepository userRepository;
     @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Override
-    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
-        return userRepository.findByLogin(login);
-    }
-
+    private  PasswordEncoder passwordEncoder;
     @Override
     public void afterPropertiesSet() throws Exception { /*post-initialization, add admin to data base*/
         if (userRepository.findByLogin(ADMIN_LOGIN) == null)
             userRepository.save(new User(null, ADMIN_LOGIN, passwordEncoder.encode(ADMIN_PASSWORD), "ADMIN"));
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByLogin(username);
     }
 }
