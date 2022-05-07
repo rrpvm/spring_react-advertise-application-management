512ec5d4035f6b6a11bcad26d3b10e0a8571f5ff
rrpvm <funikovsasa@gmail.com>
2022-05-07 14:34:13 +0700

    add new selection method

--- backend/src/main/java/com/rrpvm/backend/daos/BannerRepository.java
+++ backend/src/main/java/com/rrpvm/backend/daos/BannerRepository.java
@@ -2,11 +2,12 @@ package com.rrpvm.backend.daos;
 
 import com.rrpvm.backend.daos.custom.CustomizeBannerRepository;
 import com.rrpvm.backend.entities.Banner;
+import com.rrpvm.backend.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 public interface BannerRepository extends JpaRepository<Banner, Long>, CustomizeBannerRepository {
     Banner findBannerByName(String name);
 
-
     List<Banner> findAllByIsDeleted(boolean deleted);
+    List<Banner> findAllByCategoriesAndIsDeleted(Category linkedCategories,boolean deleted);//находит все баннеры,прикрепленные к определенной категории
 }
