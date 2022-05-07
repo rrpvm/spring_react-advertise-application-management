f550e9c69c3ee9dd4f605dc916ed012504ec0d2d
rrpvm <funikovsasa@gmail.com>
2022-05-07 14:35:02 +0700

    nothing

--- backend/src/main/java/com/rrpvm/backend/filters/JwtTokenAuthorizationOncePerRequestFilter.java
+++ backend/src/main/java/com/rrpvm/backend/filters/JwtTokenAuthorizationOncePerRequestFilter.java
@@ -1,47 +1,39 @@
 package com.rrpvm.backend.filters;
 
 import java.io.IOException;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.rrpvm.backend.exceptions.AuthenticationFailException;
-import com.rrpvm.backend.handlers.ExceptionTranslator;
-import com.rrpvm.backend.jwt.JwtTokenUtil;
-import io.jsonwebtoken.MalformedJwtException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.rrpvm.backend.daos.UserRepository;
+import com.rrpvm.backend.services.JdbcUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.filter.OncePerRequestFilter;
-
+import com.rrpvm.backend.jwt.JwtTokenUtil;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.ExpiredJwtException;
-import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
 
 @Component
 public class JwtTokenAuthorizationOncePerRequestFilter extends OncePerRequestFilter {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
-    private UserDetailsService jwtInMemoryUserDetailsService;
+    private UserRepository userRepository;
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
-    @Autowired
-    @Qualifier("handlerExceptionResolver")
-    private HandlerExceptionResolver resolver;
     @Value("${jwt.http.request.header}")
     private String tokenHeader;
 
@@ -67,7 +59,7 @@ public class JwtTokenAuthorizationOncePerRequestFilter extends OncePerRequestFil
         }
         logger.debug("JWT_TOKEN_USERNAME_VALUE '{}'", username);
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = this.jwtInMemoryUserDetailsService.loadUserByUsername(username);
+            UserDetails userDetails = this.userRepository.findByLogin(username);
             if (jwtTokenUtil.validateToken(jwtToken, userDetails)) {
                 UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
@@ -78,7 +70,6 @@ public class JwtTokenAuthorizationOncePerRequestFilter extends OncePerRequestFil
             chain.doFilter(request, response);
         } catch (ExpiredJwtException e) {
             logger.error("Spring Security Filter Chain Exception:", e);
-            resolver.resolveException(request, response, null, e);//send exception to exception translator
         }
     }
 }
